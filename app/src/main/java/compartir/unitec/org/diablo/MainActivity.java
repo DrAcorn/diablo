ba002d0d0b60c363cf5b4c13d127ae217bf27152
DrAcorn <electric.eye09@gmail.com>
2018-02-08 17:46:04 -0600

    tercero

--- app/src/main/java/compartir/unitec/org/diablo/MainActivity.java
+++ app/src/main/java/compartir/unitec/org/diablo/MainActivity.java
@@ -2,6 +2,8 @@ package compartir.unitec.org.diablo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+            findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(),"Tu mensaje se envio", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
